7a09b46e31a14e393b7fa61586d82bed5f1f09be
Zach Wegrzyniak <zach@streak.com>
2024-05-06 09:10:36 -0400

    create repro example

---
+++ index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const web_extension_1 = require("firebase/auth/web-extension");
+console.log(web_extension_1.getAuth, web_extension_1.signInWithCustomToken);
---
+++ index.ts
@@ -0,0 +1,3 @@
+import { getAuth, signInWithCustomToken } from "firebase/auth/web-extension";
+
+console.log(getAuth, signInWithCustomToken);
